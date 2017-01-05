83c13c6d566efffec2d931812651ad2c2263594c
Brian Zelip <bzelip@gmail.com>
2017-01-05 15:54:01 -0500

    clean up and externalize js; about to merge into master

--- day2/clock.js
+++ day2/clock.js
@@ -1,33 +1,33 @@
-// the set up
-const s = document.querySelector('.hand-sec');
-const m = document.querySelector('.hand-min');
-const h = document.querySelector('.hand-hr');
+const s = document.querySelector('.second');
+const m = document.querySelector('.minute');
+const h = document.querySelector('.hour');
 
-const ts = new Date();
+function setTime() {
+  const now = new Date();
 
-const rotS = (360/60)*ts.getSeconds();
-const rotM = (360/60)*ts.getMinutes();
-const rotH = (360/12)*ts.getHours();
+  const rotS = ((now.getSeconds()/60)*360) + 90;
+  const rotM = ((now.getMinutes()/60)*360) + 90;
+  const rotH = ((now.getHours()/12)*360) + 90;
+  // 90 degrees added to orient the hands around 12:00 position instead of 9:00
+
+  s.style.transform = `rotate(${rotS}deg)`;
+  m.style.transform = `rotate(${rotM}deg)`;
+  h.style.transform = `rotate(${rotH}deg)`;
+}
+
+setInterval(setTime, 1000);
 
 
 // testing grounds
-console.log('current time: ' + ts + '\n' + '\
-current hour: ' + ts.getHours() + '\n' + '\
-current min: ' + ts.getMinutes() + '\n' + '\
-current sec: ' + ts.getSeconds() + '\n' + '\
-\n' + '\
-rotS = ' + rotS + '\n' + '\
-rotM = ' + rotM + '\n' + '\
-rotH = ' + rotH);
-
-console.log(s);
-console.log(m);
-console.log(h);
-
-
-// show time at page load
-s.style.transform = 'rotate(' + rotS + 'deg)';
-m.style.transform = 'rotate(' + rotM + 'deg)';
-h.style.transform = 'rotate(' + rotH + 'deg)';
-
-// animate hand
+// console.log('current time: ' + ts + '\n' + '\
+// current hour: ' + ts.getHours() + '\n' + '\
+// current min: ' + ts.getMinutes() + '\n' + '\
+// current sec: ' + ts.getSeconds() + '\n' + '\
+// \n' + '\
+// rotS = ' + rotS + '\n' + '\
+// rotM = ' + rotM + '\n' + '\
+// rotH = ' + rotH);
+//
+// console.log(s);
+// console.log(m);
+// console.log(h);
