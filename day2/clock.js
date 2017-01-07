c1b8c9fa0c3e4aae214812488ba3f24983a800df
Brian Zelip <bzelip@gmail.com>
2017-01-07 11:13:24 -0500

    fixed the animation reset when any hand reaches 90deg

--- day2/clock.js
+++ day2/clock.js
@@ -1,6 +1,8 @@
+
 const s = document.querySelector('.second');
 const m = document.querySelector('.minute');
 const h = document.querySelector('.hour');
+const hands = document.querySelectorAll('.hand');
 
 function setTime() {
   const now = new Date();
@@ -9,6 +11,34 @@ function setTime() {
   const rotM = ((now.getMinutes()/60)*360) + 90;
   const rotH = ((now.getHours()/12)*360) + 90;
   // 90 degrees added to orient the hands around 12:00 position instead of 9:00
+
+
+  // if statement to remove the animation reset back to square one (rotate(90deg))
+  // when any rotate value == 90 if `transition` and
+  // `transition-timing-function` are kept in css.
+  if (rotS == 90) {
+    s.style.transition = 'none';
+    s.style.transitionTimingFunction = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
+  } else if (rotS !== 90) {
+    s.style.transition = 'all 0.05s';
+    s.style.transitionTimingFunction = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
+  }
+
+  if (rotM == 90) {
+    m.style.transition = 'none';
+    m.style.transitionTimingFunction = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
+  } else if (rotM !== 90) {
+    m.style.transition = 'all 0.05s';
+    m.style.transitionTimingFunction = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
+  }
+
+  if (rotH == 90) {
+    h.style.transition = 'none';
+    h.style.transitionTimingFunction = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
+  } else if (rotH !== 90) {
+    h.style.transition = 'all 0.05s';
+    h.style.transitionTimingFunction = 'cubic-bezier(0.1, 2.7, 0.58, 1)';
+  }
 
   s.style.transform = `rotate(${rotS}deg)`;
   m.style.transform = `rotate(${rotM}deg)`;
